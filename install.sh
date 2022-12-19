68d29e355d026f9fc495462d41374384ca019734
Alexis <120563837+alexisart@users.noreply.github.com>
2022-12-19 16:55:13 -0500

    Initial Commit

---
+++ install.sh
@@ -0,0 +1,11 @@
+# Install Initial Software
+sudo apt install nodejs npm zsh git
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Edit Config Files
+sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="agnoster"/g' ~/.zshrc
+sed -i 's/plugins=(git)/plugins=(\n\tgit\n\tzsh-syntax-highlighting\n\tzsh-autosuggestions\n)/g' ~/.zshrc
+
+# Install ZSH Plugins
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
